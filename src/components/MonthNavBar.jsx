41cbf4367d78a65b331182e016cc877a711a9272
Yashraj <118662533+Dutta159@users.noreply.github.com>
2024-12-13 23:09:49 +0530

    alignment changes to month navbar

--- src/components/MonthNavBar.jsx
+++ src/components/MonthNavBar.jsx
@@ -1,7 +1,7 @@
 import { format } from "date-fns"
 const  MonthNavBar = ({ handlePrevMonth, handleNextMonth, currentDate })=>{
     return(
-        <div className="flex justify-between items-center mb-4">
+        <div className="flex justify-around items-center mb-4 w-1/2 mb-6 justify-self-center border-b-2 p-2 mt-3">
         <button onClick={handlePrevMonth} className="bg-gray-300 px-2 py-1 rounded">Previous</button>
         <h2 className="text-lg font-semibold">{format(currentDate, "MMMM yyyy")}</h2>
         <button onClick={handleNextMonth} className="bg-gray-300 px-2 py-1 rounded">Next</button>
